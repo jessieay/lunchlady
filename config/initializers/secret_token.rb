22d81f8343e8016eb9627a3fa5d4a42fc4173689
Jessie Young <jessie@apprentice.io>
2012-10-19 11:25:25 -0700

    First glorious commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchlady::Application.config.secret_token = '3ef53dd1d8ac551f9671a14e806f83ecbf19d358c2f5bd22533b0ae4c007247b51ce4ab50d32371a467a8666b8e6edb804867b56c9ec9f265c2258ef6540cb7e'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Lunchlady::Application.routes.draw do
+  root :to => 'Clearance::Sessions#new'
+
+end
